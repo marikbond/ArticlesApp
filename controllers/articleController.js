b740f5010e4e59cf2158516f6d45f0deed34199c
Mark <markb.privat@gmail.com>
2017-02-10 09:46:09 -0500

    first try of modules on function saveArticle

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,13 +1,16 @@
+(function () {
+    function saveArticle() {
+        var title = document.getElementById("article-title").value;
+        var content = document.getElementById('article-content').value;
+        var article = {
+            title: title,
+            content: content,
+            creationDate: new Date(),
+            author: ""
+        };
+        saveArticleToStorage(article);
+        addArticleToView(article);
+    }
 
-function saveArticle() {
-    var title = document.getElementById("article-title").value;
-    var content = document.getElementById('article-content').value;
-    var article = {
-        title: title,
-        content: content,
-        creationDate: new Date(),
-        author: ""
-    };
-    saveArticleToStorage(article);
-    addArticleToView(article);
-}
+    window.saveArticles = saveArticle();
+})();
